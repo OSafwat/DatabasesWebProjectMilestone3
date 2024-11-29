7ea1afe1b5fa86b96e55475cfaad6771f4b8079f
OSafwat <omarsafsab2004@gmail.com>
2024-11-29 13:41:21 +0200

    Add project files.

---
+++ DatabasesWebProjectMilestone3/Login.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="DatabasesWebProjectMilestone3.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Please Log In<br />
+            Username:<br />
+            <asp:TextBox ID="username" runat="server"></asp:TextBox>
+            <br />
+            Password:<br />
+            <asp:TextBox ID="password" runat="server"></asp:TextBox>
+            <br />
+        </div>
+        <asp:Button ID="login_button" runat="server" OnClick="login_button" Text="Log In" />
+    </form>
+</body>
+</html>
---
+++ DatabasesWebProjectMilestone3/Login.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DatabasesWebProjectMilestone3
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void login_button(object sender, EventArgs e)
+        {
+            String connStr = WebConfigurationManager.ConnectionStrings["DatabasesWebsite"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            int id = Int16.Parse(username.Text);
+            string pass = password.Text;
+            SqlCommand loginproc = new SqlCommand("userLogin", conn);
+            loginproc.Parameters.Add(new SqlParameter("@id", id));
+            loginproc.Parameters.Add(new SqlParameter("@password", pass));
+
+            SqlParameter success = loginproc.Parameters.Add("@success", SqlDbType.Int);
+            SqlParameter type = loginproc.Parameters.Add("@type", SqlDbType.Int);
+        }
+    }
+}
